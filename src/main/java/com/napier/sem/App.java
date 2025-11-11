d946ef54d68a3efe36c2f6ebca5abbb245c4aa3e
Harold Lambton <40722970@live.napier.ac.uk>
2025-11-11 18:49:07 +0000

    integration test integration

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -66,7 +66,7 @@ public class App
         App a = new App();
 
         if(args.length < 1){
-            a.connect("localhost:3306", 30000);
+            a.connect("localhost:33060", 30000);
         }else{
             a.connect(args[0], Integer.parseInt(args[1]));
         }
